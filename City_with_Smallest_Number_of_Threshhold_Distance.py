72a3c6d654ff71a196bd69343425715012035545
BigBang001 <119509166+BigBang001@users.noreply.github.com>
2025-01-05 23:03:25 +0530

    Create City_with_Smallest_Number_of_Threshhold_Distance.py

---
+++ City_with_Smallest_Number_of_Threshhold_Distance.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def findTheCity(self, n, edges, distanceThreshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type distanceThreshold: int
+        :rtype: int
+        """
+        dist = [[float('inf')] * n for _ in range(n)]
+        
+        for i in range(n):
+            dist[i][i] = 0
+        
+        for u, v, w in edges:
+            dist[u][v] = w
+            dist[v][u] = w
+       
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][k] + dist[k][j] < dist[i][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+        
+        minCount = n
+        resultCity = -1
+        
+        for i in range(n):
+            count = sum(dist[i][j] <= distanceThreshold for j in range(n))
+            if count <= minCount:
+                minCount = count
+                resultCity = i
+        
+        return resultCity
